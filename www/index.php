9de55a6c8dc0b02b981aaa2e35a75432236c7743
Tadeas Kosek <tadeas.kosek@netvor.co>
2019-07-10 16:58:10 +0200

    add index testing markup

--- www/index.php
+++ www/index.php
@@ -15,10 +15,22 @@ require_once __DIR__.'/modules/header.php';
     <main class="main">
         <section class="section">
             <div class="container grid-body">
-                <div class="span__4 span__6_l">ITEM 1</div>
-                <div class="span__4 span__6_l s-first">ITEM 2</div>
+                <div class="span__4 span__6_l justify-start-s">
+                    <div>
+                        <div>
+                            <h5>Challanges</h5>
+                            <h2>What’s happening in Mozambique</h2>
+                        </div>
+                        <p>Skate World Better is a non-profit based in Copenhagen, Denmark. Our goal is to build the first public skatepark in Maputo, Mozambique by fall 2019 in order to support Maputo’s skateboarding community. To reach our goal we’ve teamed up with Skate-aid and ASM</p>
+                        <p>Mozambique is a country located in southeast Africa experiencing a ‘youth bulge’ in its population. We believe skateboarding can be used as a tool to keep kids away from the dangers of drug use and crime, which are unfortunately prevalent in Mozambique’s capital city.</p>
+                        <p>This project will also provide the city of Maputo with a hip and resurgent place of recreation and assembly.</p>
+                    </div>
+                </div>
+                <div class="span__4 span__6_l s-first justify-center-s justify-end-m-up">
+                    <div class="img">IMG</div>
+                </div>
             </div>
-            <div class="container grid-body">
+            <div class="container grid-body justify-items-center align-items-stretch">
                 <div class="span__4">
                     <div>Challanges</div>
                 </div>
@@ -39,13 +51,145 @@ require_once __DIR__.'/modules/header.php';
                 </div>
             </div>
         </section>
-        <section>
+        <section class="section">
             <div class="container grid-body">
+                <div class="span__4 span__8_m span__4_l">
+                    <div>
+                        <div>
+                            <h5>Solutions</h5>
+                            <h2>What are we doing to help</h2>
+                        </div>
+                        <p>We came up with an interconnected project, that not only joints multiple activities, but also people of all origins with different interests.</p>
+                        <p>Altogether, we will create a youth center that will serve all kinds of purposes within the city of Maputo.</p>
+                    </div>
+                </div>
+                <div class="span__4 span__8_m">
+                    <div class="subgrid-2-col">
+                        <div class="span__4">
+                            <div>
+                                <div>
+                                    <h4>Build</h4>
+                                </div>
+                                <p>SWB, Skate-Aid and AMS will build the first public skatepark in Mozambique along with a youth center on the property. It will be right on the coast, in the suburban area of the capital city Maputo.</p>
+                                <p>It is going to be concrete, for both, skilled skaters and beginners. The park will feature a technical and playful section, implementing urban elements, but also a real bowl.</p>
+                            </div>
+                        </div>
+                        <div class="span__4">
+                            <div>
+                                <div>
+                                    <h4>Teach</h4>
+                                </div>
+                                <p>Once the skatepark is built educational seminars, workshops, concerts and exhibitions will take place on Costa do Sol in the first month. After the opening ceremony ASM together with Skate-Aid will facilitate regular skateboarding lessons.</p>
+                            </div>
+                        </div>
+                        <div class="span__4">
+                            <div>
+                                <div>
+                                    <h4>Skate</h4>
+                                </div>
+                                <p>Through skateboarding we want to offer a playful and creative way to learn and make new friendships. In skating, competitiveness is replaced by joint effort and enthusiasm for progress.</p>
+                                <p>To bolster local skate culture we are also bringing skateboards, skate gear and experience with us, to give away in Maputo.</p>
+                            </div>
+                        </div>
+                        <div class="span__4">
+                            <div>
+                                <div>
+                                    <h4>Culture</h4>
+                                </div>
+                                <p>A massive wall surrounding the property will serve as free canvas for local artists and art enthusiasts.</p>
+                                <p>Furthermore, the youth center will regularly host art exhibitions, video premieres, craft workshops, lectures, readings and much more.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
-        <section>
+        <section class="section">
+            <div class="container">
+                <div class="card-main">
+                    <div class="grid-body">
+                        <div class="span__full justify-center-s-up">
+                            <div>
+                                <h2>Achievements</h2>
+                                <p>By now we have already secured the most important essentials to make this happen.</p>
+                            </div>
+                        </div>
+                        <div class="span__4">
+                            <div>
+                                <div class="img"></div>
+                                <h4>Material Assets</h4>
+                                <p>Through our joint venture with Skate-Aid we have been able to cover the material expenses.</p>
+                            </div>
+                        </div>
+                        <div class="span__4">
+                            <div>
+                                <div class="img"></div>
+                                <h4>The land</h4>
+                                <p>The land is 20x40 meters, located in Costa do Sol, a suburban area of Maputo right by the ocean. It is connected with the centre by public bus line.</p>
+                            </div>
+                        </div>
+                        <div class="span__4 span__8_m span__4_l">
+                            <div>
+                                <div class="img"></div>
+                                <h4>Our boards</h4>
+                                <p>We have created our own design with one of our sponsors, Ambassadors skate company. Some boards will be sold to skaters around the world for funding, while the rest will be brought to Maputo for local skaters.</p>
+                            </div>
+                        </div>
+                        <div class="span__full justify-center-s-up">
+                            <div>
+                                <h2>Section heading</h2>
+                                <p>Section subtitle</p>
+                            </div>
+                        </div>
+                        <div class="span__full justify-center-s-up">
+                            <div>
+                                TO BE DECIDED
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section">
             <div class="container grid-body">
-                <button class="btn btn-block btn-sec">Button</button>
+                <div class="span__4 span__6_l">
+                    <div>
+                        <h2>Section heading</h2>
+                        <p>Section subtitle</p>
+                    </div>
+                </div>
+                <div class="span__4 span__6_l justify-end-m-up">
+                    <div class="img">IMG</div>
+                </div>
+            </div>
+            <div class="container grid-body justify-items-center">
+                <div class="span__4">
+                    <div>SPONSOR LOGO</div>
+                </div>
+                <div class="span__4">
+                    <div>SPONSOR LOGO</div>
+                </div>
+                <div class="span__4">
+                    <div>SPONSOR LOGO</div>
+                </div>
+                <div class="span__4">
+                    <div>SPONSOR LOGO</div>
+                </div>
+                <div class="span__4">
+                    <div>SPONSOR LOGO</div>
+                </div>
+                <div class="span__4">
+                    <div>SPONSOR LOGO</div>
+                </div>
+                <div class="span__4">
+                    <div>SPONSOR LOGO</div>
+                </div>
+                <div class="span__4">
+                    <div>SPONSOR LOGO</div>
+                </div>
+                <div class="span__4">
+                    <div>SPONSOR LOGO</div>
+                </div>
             </div>
         </section>
     </main>
