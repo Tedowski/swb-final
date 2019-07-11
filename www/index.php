e0b372b82e7fc628ed08cfe79279c361b36c27d0
Tadeas Kosek <tadeas.kosek@netvor.co>
2019-07-11 02:32:52 +0200

    add vertical spacing options - 8pt rule margins and paddings

--- www/index.php
+++ www/index.php
@@ -6,20 +6,20 @@ require_once __DIR__.'/modules/header.php';
 <div class="wrapper">
     <section class="hero hero__landing section">
         <div class="container grid-body">
-            <div class="span__full">
-                <h1>My App</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci commodi consectetur dolores hic magni, quis reiciendis sint ut.</p>
+            <div class="span__full justify-center-s-up">
+                <h1 class="heading">My App</h1>
+                <p class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci commodi consectetur dolores hic magni, quis reiciendis sint ut.</p>
             </div>
         </div>
     </section>
     <main class="main">
-        <section class="section">
+        <section class="section gutter-top-xl gutter-bot-l">
             <div class="container grid-body">
                 <div class="span__4 span__6_l justify-start-s">
                     <div>
                         <div>
-                            <h5>Challanges</h5>
-                            <h2>What’s happening in Mozambique</h2>
+                            <h5 class="subtitle">Challanges</h5>
+                            <h2 class="title">What’s happening in Mozambique</h2>
                         </div>
                         <p>Skate World Better is a non-profit based in Copenhagen, Denmark. Our goal is to build the first public skatepark in Maputo, Mozambique by fall 2019 in order to support Maputo’s skateboarding community. To reach our goal we’ve teamed up with Skate-aid and ASM</p>
                         <p>Mozambique is a country located in southeast Africa experiencing a ‘youth bulge’ in its population. We believe skateboarding can be used as a tool to keep kids away from the dangers of drug use and crime, which are unfortunately prevalent in Mozambique’s capital city.</p>
@@ -30,7 +30,7 @@ require_once __DIR__.'/modules/header.php';
                     <div class="img">IMG</div>
                 </div>
             </div>
-            <div class="container grid-body justify-items-center align-items-stretch">
+            <div class="container grid-body justify-items-center align-items-stretch margin-top-xl gutter-top-s">
                 <div class="span__4">
                     <div>Challanges</div>
                 </div>
@@ -51,13 +51,13 @@ require_once __DIR__.'/modules/header.php';
                 </div>
             </div>
         </section>
-        <section class="section">
+        <section class="section gutter-top-xl gutter-bot-l">
             <div class="container grid-body">
                 <div class="span__4 span__8_m span__4_l">
                     <div>
                         <div>
-                            <h5>Solutions</h5>
-                            <h2>What are we doing to help</h2>
+                            <h5 class="subtitle">Solutions</h5>
+                            <h2 class="title">What are we doing to help</h2>
                         </div>
                         <p>We came up with an interconnected project, that not only joints multiple activities, but also people of all origins with different interests.</p>
                         <p>Altogether, we will create a youth center that will serve all kinds of purposes within the city of Maputo.</p>
@@ -68,7 +68,7 @@ require_once __DIR__.'/modules/header.php';
                         <div class="span__4">
                             <div>
                                 <div>
-                                    <h4>Build</h4>
+                                    <h4 class="item-headline">Build</h4>
                                 </div>
                                 <p>SWB, Skate-Aid and AMS will build the first public skatepark in Mozambique along with a youth center on the property. It will be right on the coast, in the suburban area of the capital city Maputo.</p>
                                 <p>It is going to be concrete, for both, skilled skaters and beginners. The park will feature a technical and playful section, implementing urban elements, but also a real bowl.</p>
@@ -77,7 +77,7 @@ require_once __DIR__.'/modules/header.php';
                         <div class="span__4">
                             <div>
                                 <div>
-                                    <h4>Teach</h4>
+                                    <h4 class="item-headline">Teach</h4>
                                 </div>
                                 <p>Once the skatepark is built educational seminars, workshops, concerts and exhibitions will take place on Costa do Sol in the first month. After the opening ceremony ASM together with Skate-Aid will facilitate regular skateboarding lessons.</p>
                             </div>
@@ -85,7 +85,7 @@ require_once __DIR__.'/modules/header.php';
                         <div class="span__4">
                             <div>
                                 <div>
-                                    <h4>Skate</h4>
+                                    <h4 class="item-headline">Skate</h4>
                                 </div>
                                 <p>Through skateboarding we want to offer a playful and creative way to learn and make new friendships. In skating, competitiveness is replaced by joint effort and enthusiasm for progress.</p>
                                 <p>To bolster local skate culture we are also bringing skateboards, skate gear and experience with us, to give away in Maputo.</p>
@@ -94,7 +94,7 @@ require_once __DIR__.'/modules/header.php';
                         <div class="span__4">
                             <div>
                                 <div>
-                                    <h4>Culture</h4>
+                                    <h4 class="item-headline">Culture</h4>
                                 </div>
                                 <p>A massive wall surrounding the property will serve as free canvas for local artists and art enthusiasts.</p>
                                 <p>Furthermore, the youth center will regularly host art exhibitions, video premieres, craft workshops, lectures, readings and much more.</p>
@@ -104,41 +104,41 @@ require_once __DIR__.'/modules/header.php';
                 </div>
             </div>
         </section>
-        <section class="section">
+        <section class="section gutter-top-xl gutter-bot-l">
             <div class="container">
                 <div class="card-main">
                     <div class="grid-body">
-                        <div class="span__full justify-center-s-up">
-                            <div>
-                                <h2>Achievements</h2>
-                                <p>By now we have already secured the most important essentials to make this happen.</p>
+                        <div class="span__full">
+                            <div class="text-centered">
+                                <h2 class="title">Achievements</h2>
+                                <p class="subtitle">By now we have already secured the most important essentials to make this happen.</p>
                             </div>
                         </div>
                         <div class="span__4">
                             <div>
                                 <div class="img"></div>
-                                <h4>Material Assets</h4>
+                                <h4 class="item-headline">Material Assets</h4>
                                 <p>Through our joint venture with Skate-Aid we have been able to cover the material expenses.</p>
                             </div>
                         </div>
                         <div class="span__4">
                             <div>
                                 <div class="img"></div>
-                                <h4>The land</h4>
+                                <h4 class="item-headline">The land</h4>
                                 <p>The land is 20x40 meters, located in Costa do Sol, a suburban area of Maputo right by the ocean. It is connected with the centre by public bus line.</p>
                             </div>
                         </div>
                         <div class="span__4 span__8_m span__4_l">
                             <div>
                                 <div class="img"></div>
-                                <h4>Our boards</h4>
+                                <h4 class="item-headline">Our boards</h4>
                                 <p>We have created our own design with one of our sponsors, Ambassadors skate company. Some boards will be sold to skaters around the world for funding, while the rest will be brought to Maputo for local skaters.</p>
                             </div>
                         </div>
-                        <div class="span__full justify-center-s-up">
-                            <div>
-                                <h2>Section heading</h2>
-                                <p>Section subtitle</p>
+                        <div class="span__full">
+                            <div class="text-centered">
+                                <h2 class="title">Section heading</h2>
+                                <p class="subtitle">Section subtitle</p>
                             </div>
                         </div>
                         <div class="span__full justify-center-s-up">
@@ -150,19 +150,19 @@ require_once __DIR__.'/modules/header.php';
                 </div>
             </div>
         </section>
-        <section class="section">
+        <section class="section gutter-top-xl gutter-bot-l">
             <div class="container grid-body">
                 <div class="span__4 span__6_l">
                     <div>
-                        <h2>Section heading</h2>
-                        <p>Section subtitle</p>
+                        <h5 class="subtitle">Partnerships</h5>
+                        <h2 class="title">Alone we can do so little, together we can do so much.</h2>
                     </div>
                 </div>
                 <div class="span__4 span__6_l justify-end-m-up">
                     <div class="img">IMG</div>
                 </div>
             </div>
-            <div class="container grid-body justify-items-center">
+            <div class="container grid-body justify-items-center margin-top-xl gutter-top-s">
                 <div class="span__4">
                     <div>SPONSOR LOGO</div>
                 </div>
--- www/modules/footer.php
+++ www/modules/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer class="footer gutter-bot-l gutter-top-l">
     <div class="container grid-body">
         <h1>FOOTER</h1>
     </div>
