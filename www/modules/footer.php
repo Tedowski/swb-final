4dbbd27638c8dab4af60a2b02bbe786ac851b72a
Tadeas Kosek <teddcz@gmail.com>
2019-08-28 22:02:45 +0200

    add 404 page with redirect

--- www/modules/footer.php
+++ www/modules/footer.php
@@ -1,35 +1,43 @@
 <footer class="footer gutter-bot-m gutter-top-l">
     <div class="container grid-body">
         <div class="span__4 span__3_l">
-            <h5>Skate world better</h5>
-            <p>Skate world better raises money for Mozambique and it's citizens. It is a young, innovative and charitable organization</p>
-            <div class="icon-circ">F</div>
-            <div class="icon-circ">IG</div>
-        </div>
-        <div class="span__4 span__3_l">
-            <h5>Sitemap</h5>
-            <div class="subnav">
-                <a href="./index.php" class="subnav-link">Home</a>
-                <a href="./project.php" class="subnav-link">Project</a>
-                <a href="./about.php" class="subnav-link">About us</a>
-                <a href="./blog.php" class="subnav-link">Blog</a>
-                <a href="./contact.php" class="subnav-link">Contact</a>
+            <div class="footer__item">
+                <h5>Skate world better</h5>
+                <p>Skate world better raises money for Mozambique and it's citizens. It is a young, innovative and charitable organization</p>
+                <div class="icon-circ">F</div>
+                <div class="icon-circ">IG</div>
             </div>
         </div>
         <div class="span__4 span__3_l">
-            <h5>Contact</h5>
-            <div class="contact-item">Copenhagen, Denmark</div>
-            <div class="contact-item">+45 91 61 92 31</div>
-            <div class="contact-item">info@swb.dk</div>
+            <div class="footer__item">
+                <h5>Sitemap</h5>
+                <div class="subnav">
+                    <a href="./index.php" class="subnav-link">Home</a>
+                    <a href="./project.php" class="subnav-link">Project</a>
+                    <a href="./about.php" class="subnav-link">About us</a>
+                    <a href="./blog.php" class="subnav-link">Blog</a>
+                    <a href="./contact.php" class="subnav-link">Contact</a>
+                </div>
+            </div>
         </div>
         <div class="span__4 span__3_l">
-            <h5>Get updates about the cause</h5>
-            <form class="form">
-                <div class="input-wrapper">
-                    <input class="input" type="text" placeholder="Your E-mail">
-                    <button class="btn btn-prim btn-block input-append">Sign up</button>
-                </div>
-            </form>
+            <div class="footer__item">
+                <h5>Contact</h5>
+                <div class="contact-item">Copenhagen, Denmark</div>
+                <div class="contact-item">+45 91 61 92 31</div>
+                <div class="contact-item">info@swb.dk</div>
+            </div>
+        </div>
+        <div class="span__4 span__3_l">
+            <div class="footer__item">
+                <h5>Get updates about the cause</h5>
+                <form class="form">
+                    <div class="input-wrapper">
+                        <input class="input" type="text" placeholder="Your E-mail">
+                        <button class="btn btn-prim btn-block input-append">Sign up</button>
+                    </div>
+                </form>
+            </div>
         </div>
         <div class="span__full">
             <hr>
