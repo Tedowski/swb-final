7b08954972901f2b73b7881d4668e617ee16fb7e
Tadeas Kosek <teddcz@gmail.com>
2019-08-28 21:48:12 +0200

    markup footer

--- www/modules/footer.php
+++ www/modules/footer.php
@@ -1,5 +1,39 @@
-<footer class="footer gutter-bot-l gutter-top-l">
+<footer class="footer gutter-bot-m gutter-top-l">
     <div class="container grid-body">
-        <h1>FOOTER</h1>
+        <div class="span__4 span__3_l">
+            <h5>Skate world better</h5>
+            <p>Skate world better raises money for Mozambique and it's citizens. It is a young, innovative and charitable organization</p>
+            <div class="icon-circ">F</div>
+            <div class="icon-circ">IG</div>
+        </div>
+        <div class="span__4 span__3_l">
+            <h5>Sitemap</h5>
+            <div class="subnav">
+                <a href="./index.php" class="subnav-link">Home</a>
+                <a href="./project.php" class="subnav-link">Project</a>
+                <a href="./about.php" class="subnav-link">About us</a>
+                <a href="./blog.php" class="subnav-link">Blog</a>
+                <a href="./contact.php" class="subnav-link">Contact</a>
+            </div>
+        </div>
+        <div class="span__4 span__3_l">
+            <h5>Contact</h5>
+            <div class="contact-item">Copenhagen, Denmark</div>
+            <div class="contact-item">+45 91 61 92 31</div>
+            <div class="contact-item">info@swb.dk</div>
+        </div>
+        <div class="span__4 span__3_l">
+            <h5>Get updates about the cause</h5>
+            <form class="form">
+                <div class="input-wrapper">
+                    <input class="input" type="text" placeholder="Your E-mail">
+                    <button class="btn btn-prim btn-block input-append">Sign up</button>
+                </div>
+            </form>
+        </div>
+        <div class="span__full">
+            <hr>
+            <p class="margin-top-m">Copyright 2019 Tripleton reserved</p>
+        </div>
     </div>
 </footer>
