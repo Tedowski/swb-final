c62fd3813e23c42fdfb47fd89a8949899673e471
Tadeas Kosek <teddcz@gmail.com>
2019-09-11 23:52:36 +0200

    style error page and add favicons

--- www/404.php
+++ www/404.php
@@ -10,9 +10,11 @@ require_once __DIR__.'/modules/header.php';
         <div class="container grid-body">
             <div class="span__full">
                 <div class="text-centered">
-                    <h1 class="title">404</h1>
-                    <h3 class="headline">Something went wrong</h3>
-                    <a class="btn btn-inline btn-inline__medium btn-prim margin-top-s" href="./index.php">Go home</a>
+                    <h1 class="error_code">404</h1>
+                    <h3 class="error_title">Ooops! Something went wrong</h3>
+                    <p class="error_expl">Seems like the page you are looking for does not exist</p>
+                    <a class="btn btn-inline btn-inline__medium btn-prim margin-top-s" onclick="history.go(-1)">Go back</a>
+                    <a class="btn btn-inline btn-inline__medium btn-sec margin-top-s" href="./index.php">Go home</a>
                 </div>
             </div>
         </div>
