2cbeeb5872ea46e331ef2e6df3dbacdae028463c
Tadeas Kosek <teddcz@gmail.com>
2019-09-16 02:11:47 +0200

    add open graphs and fix header logo bug

--- www/modules/top.php
+++ www/modules/top.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <link rel="canonical" href="http://swb.dk/canonical-link.html"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
@@ -13,6 +14,15 @@
     <meta name="theme-color" content="#3e9cbb">
     <script src="https://kit.fontawesome.com/1c4f9624b2.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="Skate world better">
+    <meta property="og:site_name" content="SWB">
+    <meta property="og:url" content="https://swb.dk/">
+    <meta property="og:image" content="http://swb.dk/images/hero.png">
+    <meta name="description" content="Skate World Better is a non-profit based in Copenhagen, Denmark. Our goal is to build the first public skatepark in Maputo, Mozambique by fall 2019 in order to">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Skate world better">
+    <meta property="article:published_time" content="2019-03-31">
     <script src="https://unpkg.com/scrollreveal"></script>
     <link rel="stylesheet" href="./styles/index.css">
     <title>Skate World Better - Building first skatepark in Mozambique</title>
